bcc2c82e46c4683521d5a4027e5ab01716558829
Negm <youssefkhalednegm24@gmail.com>
2025-07-13 20:50:22 +0300

    chore: updated DB config from root to negm user

--- backend/config.py
+++ backend/config.py
@@ -1,1 +1,11 @@
-#  condig.py will be made soon
+import os
+
+class Config:
+    # Database
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "mysql://negm:Yoyoqls_2005@localhost/campuspulse")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # JWT
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret-key")
+
+    # Later: email config, CORS, etc.
