42218333a552f77ff850f88575af101cdcce22c2
Negm <youssefkhalednegm24@gmail.com>
2025-07-13 19:39:25 +0300

    feat: added flask app in app/init.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,1 +1,16 @@
-#  init.py will be made soon
+from flask import Flask
+from .extensions import db, jwt  # <-- We'll create this file in the next step
+
+def create_app():
+    app = Flask(__name__)
+    
+    # Load configuration from config.py
+    app.config.from_object("config.Config")
+
+    # Initialize extensions with this app
+    db.init_app(app)
+    jwt.init_app(app)
+
+    # Blueprints will be registered later
+
+    return app
